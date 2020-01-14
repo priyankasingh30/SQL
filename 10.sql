2729c7b8794787209a8e090688eb6c7e5cdc0662
priyankasingh30 <59786324+priyankasingh30@users.noreply.github.com>
2020-01-13 20:43:39 -0800

    Length problem

---
+++ 10.sql
@@ -0,0 +1,16 @@
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+
+select a.city, a.len
+from
+(select city, length(city) as len
+ from station
+ order by len asc, city asc) a
+where rownum = 1
+union 
+select a.city, a.len
+from
+(select city, length(city) as len
+ from station
+ order by len desc, city desc) a
+where rownum = 1;
+
