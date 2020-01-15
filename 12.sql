13972e20b4c3bf10aa6800cecd1157a3e5e6772e
priyankasingh30 <59786324+priyankasingh30@users.noreply.github.com>
2020-01-14 21:03:35 -0800

    vowel problem

---
+++ 12.sql
@@ -0,0 +1,5 @@
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+select distinct city
+from station
+where substr(city,-1,1) in ('a','e','i','o','u');
