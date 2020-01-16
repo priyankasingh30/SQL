23dbdb2ce750733ecc00bbb55cc57967a6e01c37
priyankasingh30 <59786324+priyankasingh30@users.noreply.github.com>
2020-01-15 22:52:42 -0800

    concat problem

--- 21.sql
+++ 21.sql
@@ -1,5 +1,5 @@
 --Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
-Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+--Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
 
 SELECT concat(concat(CONCAT(Name, '('), SUBSTR(Occupation,1,1)),')') 
 FROM OCCUPATIONS
